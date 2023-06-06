a9db1960548490f988ff1313cd4ba387546d04b6
GA9BR1 <gustavoalberttodev@gmail.com>
2023-06-06 16:00:29 -0300

    Fix dos warnings do rubocop

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :card do
-    number { "MyString" }
-    cpf { "MyString" }
+    number { 'MyString' }
+    cpf { 'MyString' }
     points { 1 }
     status { 1 }
     card_type { nil }
--- spec/factories/company_card_types.rb
+++ spec/factories/company_card_types.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :company_card_type do
     status { 1 }
-    cnpj { "MyString" }
+    cnpj { 'MyString' }
     card_type { nil }
-    conversion_tax { "9.99" }
+    conversion_tax { '9.99' }
   end
 end
