f815a2a21c1410e450840561b8e61fc5415a4500
GA9BR1 <gustavoalberttodev@gmail.com>
2023-06-08 10:13:33 -0300

    Adoção do scope no model card_type
    Co-authored-by: noliv197 <noliv197@gmail.com>

--- app/models/card_type.rb
+++ app/models/card_type.rb
@@ -2,11 +2,6 @@ class CardType < ApplicationRecord
   validates :name, :icon, :start_points, presence: true
   validates :name, :icon, uniqueness: true
 
-  def self.enabled
-    CardType.where(emission: true)
-  end
-
-  def self.disabled
-    CardType.where(emission: false)
-  end
+  scope :enabled, -> { where(emission: true) }
+  scope :disabled, -> { where(emission: false) }
 end
