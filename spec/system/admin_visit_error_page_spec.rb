cd25abb0975e6f38f8802e092804ada88618bc16
Luqui <luizquintanilha1987@gmail.com>
2023-06-28 17:14:37 -0300

    correção dos apontamentos feito na pr
    Co-authored-by: Rafael <rafaeldejesuspp@gmail.com>

--- spec/system/admin_visit_error_page_spec.rb
+++ spec/system/admin_visit_error_page_spec.rb
@@ -2,21 +2,12 @@ require 'rails_helper'
 
 describe 'Quando ocorre um erro na aplicação' do
   it 'retorna a página do erro 404' do
-    allow_any_instance_of(HomeController).to receive(:index).and_raise(ActiveRecord::RecordNotFound)
     admin = FactoryBot.create(:admin)
 
     login_as admin
     visit root_path
+    visit card_type_path(99)
 
     expect(page).to have_content("The page you were looking for doesn't exist.")
-  end
-  it 'retorna a página do erro 500' do
-    allow_any_instance_of(HomeController).to receive(:index).and_raise(Faraday::ConnectionFailed)
-    admin = FactoryBot.create(:admin)
-
-    login_as admin
-    visit root_path
-
-    expect(page).to have_content("We're sorry, but something went wrong.")
   end
 end
